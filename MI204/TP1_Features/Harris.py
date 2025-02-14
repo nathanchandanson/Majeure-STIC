1affb731f93dd21cab2df006075a604b476e776b
nathanchandanson <54677494+nathanchandanson@users.noreply.github.com>
2025-02-14 22:07:26 +0100

    Added TP1 - Ex3 of MI204

--- MI204/TP1_Features/Harris.py
+++ MI204/TP1_Features/Harris.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from scipy.ndimage import gaussian_filter
 
 from matplotlib import pyplot as plt
 
@@ -13,9 +14,31 @@ print("Type de l'image :",img.dtype)
 t1 = cv2.getTickCount()
 Theta = cv2.copyMakeBorder(img,0,0,0,0,cv2.BORDER_REPLICATE)
 # Mettre ici le calcul de la fonction d'intérêt de Harris
-#
-#
-#
+# Calcul de Xi
+W_size = 10
+alpha = 0.06
+sigma = 1
+
+Ix = gaussian_filter(img, sigma, order=[1,0])
+Iy = gaussian_filter(img, sigma, order=[0,1])
+IxIy = np.multiply(Ix,Iy)
+
+for x in range (h):
+    for y in range(w):
+        # Extract the sub-matrices corresponding to the window
+        Ix_window = Ix[max(0,x-W_size//2):min(h,x+W_size//2), max(0,y-W_size//2):min(w,y+W_size//2)]
+        # Ix_window = gaussian_filter(Ix_window, sigma = sigma_2)
+        Iy_window = Iy[max(0,x-W_size//2):min(h,x+W_size//2), max(0,y-W_size//2):min(w,y+W_size//2)]
+        # Iy_window = gaussian_filter(Iy_window, sigma = sigma_2)
+        IxIy_window = IxIy[max(0,x-W_size//2):min(h,x+W_size//2), max(0,y-W_size//2):min(w,y+W_size//2)]
+        # IxIy_window = gaussian_filter(IxIy_window, sigma = sigma_2)
+        Xi_top_left = np.sum(np.square(Ix_window))
+        Xi_top_right = np.sum(IxIy_window)
+        Xi_bot_right = np.sum(np.square(Iy_window))
+        Theta[x][y] = Xi_top_left*Xi_bot_right - Xi_top_right**2 - alpha*(Xi_top_left+Xi_bot_right)**2
+
+
+
 # Calcul des maxima locaux et seuillage
 Theta_maxloc = cv2.copyMakeBorder(Theta,0,0,0,0,cv2.BORDER_REPLICATE)
 d_maxloc = 3
