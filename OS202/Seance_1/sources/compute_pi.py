df91b49dec77ea0060179b61ac692a38e876e73b
nathanchandanson <nathan.chandanson@ensta.org>
2025-02-16 10:24:48 +0100

    Corrected the compute_pi.py (send numpy arrays) and added the Ex1 of TP2 of OS202

--- OS202/Seance_1/sources/compute_pi.py
+++ OS202/Seance_1/sources/compute_pi.py
@@ -9,8 +9,10 @@ nb_samples = 40_000_000
 def calcul_pi(nb_samples):
     beg = time.time()
     # Tirage des points (x,y) tirés dans un carré [-1;1] x [-1; 1]
-    x = 2.*np.random.random_sample((nb_samples,))-1.
-    y = 2.*np.random.random_sample((nb_samples,))-1.
+    # x = 2.*np.random.random_sample((nb_samples,))-1.
+    # y = 2.*np.random.random_sample((nb_samples,))-1.
+    x = 2.0 * np.random.random_sample(nb_samples) - 1.0
+    y = 2.0 * np.random.random_sample(nb_samples) - 1.0
     # Création masque pour les points dans le cercle unité
     filtre = np.array(x*x + y*y < 1.)
     # Compte le nombre de points dans le cercle unité
@@ -35,27 +37,31 @@ name    = MPI.Get_processor_name()
 # Process 0
 if(rank == 0):
     # Nbr de points par process
-    N_perProcess = nb_samples // nbp
+    N_perProcess = np.array([nb_samples // nbp], dtype='i')
     # Envoyer ce nombre de points à tt le monde 
     for i in range(1, nbp):
         globCom.Send(N_perProcess, dest = i, tag = 0)
     
     # Résultats
     # Propre résultat 
-    results = [calcul_pi(N_perProcess)]
+    pi = calcul_pi(N_perProcess)
+    results = [pi]
     # Résultats des autres
     while(len(results) < nbp):
-        temp = 0.0
+        temp = np.empty(1, dtype='float')
         globCom.Recv(temp, source = MPI.ANY_SOURCE, tag = MPI.ANY_TAG)
+        results.append(temp[0])
     
     # Calcul de Pi
+    print(results)
     res = sum(results)
     res = res / nbp
 
     print("Résultats : Pi = ", res)
 # Autres process
 else:
-    n_points = 0
+    n_points = np.empty(1, dtype = 'i')
     globCom.Recv(n_points, source = 0, tag = MPI.ANY_TAG)
-
-    globCom.Send(calcul_pi(n_points), dest = 0, tag = 0)
+    pi = calcul_pi(n_points[0])
+    msg = np.array([pi], dtype='float')
+    globCom.Send(pi, dest = 0, tag = 0)
--- OS202/Seance_2/mandelbrot_parallel.py
+++ OS202/Seance_2/mandelbrot_parallel.py
@@ -5,7 +5,7 @@ from PIL import Image
 from math import log
 from time import time
 import matplotlib.cm
-
+from mpi4py import MPI
 
 @dataclass
 class MandelbrotSet:
@@ -51,28 +51,77 @@ class MandelbrotSet:
 mandelbrot_set = MandelbrotSet(max_iterations=50, escape_radius=10)
 width, height = 1024, 1024
 
-# scaleX = 3./width
-# scaleY = 2.25/height
-# convergence = np.empty((width, height), dtype=np.double)
-# # Calcul de l'ensemble de mandelbrot :
-# deb = time()
-# for y in range(height):
-#     for x in range(width):
-#         c = complex(-2. + scaleX*x, -1.125 + scaleY * y)
-#         convergence[x, y] = mandelbrot_set.convergence(c, smooth=True)
-# fin = time()
-# print(f"Temps du calcul de l'ensemble de Mandelbrot : {fin-deb}")
+scaleX = 3./width
+scaleY = 2.25/height
+
+# Résultats totaux
+convergence = np.empty((width, height), dtype=np.double)
 
 ########## Principe ##########
 # Processus 0 : Gère la comm et calcule aussi
 # Processus >0 : calcule
 
-########## Processus 0 ##########
-# 
+# Récupération des données de MPI
+globCom = MPI.COMM_WORLD.Dup()
+nbp     = globCom.size
+rank    = globCom.rank
+name    = MPI.Get_processor_name()
 
-# Constitution de l'image résultante :
-deb = time()
-image = Image.fromarray(np.uint8(matplotlib.cm.plasma(convergence.T)*255))
-fin = time()
-print(f"Temps de constitution de l'image : {fin-deb}")
-image.save("result.png")
+########## Processus 0 ##########
+# Il doit :
+# - Choisir combien de colonnes fait chaque processus
+# - Envoyer à chaque process sa colonne de début et de fin
+# - Faire son calcul
+# - Réceptionner toutes les données des autres processus
+# - Les concaténer
+# - afficher le résultat
+if(rank == 0):
+    # Choisir le nombre de colonnes par process
+    nb_col_per_process = width//nbp
+
+    deb = time()
+
+    # Envoie des colonnes de début et de fin à chaque process
+    for i in range(1, nbp):
+        interval_to_send = np.array([i*nb_col_per_process, (i+1)*nb_col_per_process-1], dtype='int32')
+        globCom.Send(interval_to_send, dest=i, tag=0)
+    
+    # Calcul des dernières colonnes
+    result = np.empty((nb_col_per_process, height), dtype = np.double)
+    for y in range(height):
+        for x in range(nb_col_per_process):
+            c = complex(-2. + scaleX*x, -1.125 + scaleY * y)
+            result[x, y] = mandelbrot_set.convergence(c, smooth=True)
+
+########## Processus >0 ##########
+# Il doit :
+# - Récupérer les colonnes qu'il doit faire
+# - Faire son calcul
+# - Envoyer son résultat
+else:
+    # Récup des colonnes
+    interval_to_treat = np.empty(2, dtype='int32')
+    globCom.Recv(interval_to_treat, source=0, tag=MPI.ANY_TAG)
+    interval_width = interval_to_treat[1]-interval_to_treat[0]+1
+
+    # Déf du résultat
+    result = np.empty((interval_width, height), dtype = np.double)
+    # Calcul des colonnes
+    for y in range(height):
+        for i in range(interval_width):
+            x = interval_to_treat[0] + i
+            c = complex(-2. + scaleX*x, -1.125 + scaleY * y)
+            result[i, y] = mandelbrot_set.convergence(c, smooth=True)
+
+########## Gather des données ##########
+globCom.Gather(result, convergence, 0)
+
+if(rank == 0):
+    fin = time()
+    print(f"Temps du calcul de l'ensemble de Mandelbrot : {fin-deb}")
+    # Constitution de l'image résultante :
+    deb = time()
+    image = Image.fromarray(np.uint8(matplotlib.cm.plasma(convergence.T)*255))
+    fin = time()
+    print(f"Temps de constitution de l'image : {fin-deb}")
+    image.save("result.png")
