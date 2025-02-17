a30ad30b544d1a1996def48c3232a8723e9aac02
nathanchandanson <nathan.chandanson@ensta.org>
2025-02-17 08:46:28 +0100

    Started the bucket_sort.py in TP3 of OS202

--- OS202/Seance_3/bucket_sort.py
+++ OS202/Seance_3/bucket_sort.py
@@ -8,38 +8,19 @@ rank    = globCom.rank
 name    = MPI.Get_processor_name()
 
 ####### Données 
-N = int(1E4)            # Nombre de données par processus
+N = int(1E2)            # Nombre de données par processus
 max_value = int(1E6)    # Données aléatoires comprises entre [-max_value, max_value]
 
 ####### Processus 0 #######
 # Il doit :
 # - Créer son propre tableau
 # - Calculer son propre tableau [min,max,mediane]
-# - Attendre les tableaux [min,max,mediane] des autres tableaux (all gather ?)
+# - Attendre les tableaux [min,max,mediane] des autres tableaux (gather)
 # - Créer la répartition des tableaux
 # - Envoyer cette répartition à tt le monde (all broadcast)
 # - Envoyer toutes les données aux autres process correspondant à leur bucket
 # - Réceptionner toutes les valeurs de son bucket
 # - Trier son bucket
-if(rank == 0):
-    # Création de son tableau propre
-    values = np.random.randint(-max_value, max_value, size=N)
-    # Calculer son propre tableau [min,max,mediane]
-    minMaxTable = np.array([min(values), max(values), np.median(values)])
-    # Attendre les tableaux [min,max,mediane] des autres tableaux
-    while(len(minMaxTable) < 3*nbp):
-        temp = np.zeros(3)
-        globCom.Recv()
-    # Créer la répartition des tableaux
-
-    # Envoyer cette répartition à tt le monde
-
-    # Envoyer toutes les données aux autres process correspondant à leur bucket
-
-    # Réceptionner toutes les valeurs de son bucket
-
-    # Trier son bucket
-
 
 ####### Processus >0 #######
 # Il doit :
@@ -50,17 +31,45 @@ if(rank == 0):
 # - Envoyer toutes les données aux autres process correspondant à leur bucket
 # - Réceptionner toutes les valeurs de son bucket
 # - Trier le bucket
+
+####### Création des values et du minMaxTable #######
+# Création de son tableau propre
+values = np.random.randint(-max_value, max_value, size=N)
+# Calculer son propre tableau [min,max,mediane]
+minMaxTable = np.array([min(values), max(values), np.median(values)]) ######################## Modifier pour donner tous les quantiles
+
+####### Création du buffer #######
+if(rank == 0):
+    concatenated_minMaxTable = np.empty(nbp*3, dtype='float')
 else:
-    # Création de son tableau propre
-    values = np.random.randint(-max_value, max_value, size=N)
-    # Trouver le tableau [min,max,mediane] de ses données
-    minMaxTable = [min(values), max(values), np.median(values)]
-    # L'envoyer à P0
-    # globCom.
-    # Attendre la répartition des données envoyée par P0
+    concatenated_minMaxTable = None
 
-    # Envoyer toutes les données aux autres process correspondant à leur bucket
+####### Gather des données #######
+globCom.Gather(minMaxTable, concatenated_minMaxTable)
 
-    # Réceptionner toutes les valeurs de son bucket
+####### Calcul des quantiles et min/max #######
+if(rank == 0):   
+    # Créer la répartition des tableaux
+    quantiles = np.quantile(concatenated_minMaxTable, [i/nbp for i in range(1,nbp)]).tolist()
+    data_to_send = [min(concatenated_minMaxTable)] + quantiles + [max(concatenated_minMaxTable)]
+    data_to_send = np.array(data_to_send * nbp)
+else:
+    data_to_send = None
 
-    # Trier le bucket
+# Réception des tables avec min/quantiles/max
+glob_minMaxTable = np.empty(nbp+1)
+
+# Envoyer cette répartition à tt le monde
+globCom.Scatter(data_to_send, glob_minMaxTable, root=0)
+
+####### Envoie des données aux autres bucket + Réception pour notre bucket #######
+for val in values:
+    # On vérifie si la valeur est pas déjà dans le bon bucket
+    if(not (val >= glob_minMaxTable[rank] and val < glob_minMaxTable[rank+1])):
+        for i in range(len(glob_minMaxTable)-1):
+            if(val >= glob_minMaxTable[i] and val < glob_minMaxTable[i+1]):
+                # Envoyer au process correspondant
+
+
+
+####### Tri de notre bucket #######
